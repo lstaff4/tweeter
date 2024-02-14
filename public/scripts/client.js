1fdcd0e50aac8520ad9f7a6a069fbddf0a2adf23
Louis Stafford <louiststafford@gmail.com>
2024-02-14 21:11:07 +0000

    Add full tweet roster. Apply tweet length limits.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,30 +6,63 @@
 
 // Fake data taken from initial-tweets.json
 $(document).ready(function(){
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
+
+  const spaceNormalizer = function(string) {
+    let returnString = ''
+    for (i = 0; i < string.length; i++) {
+      if (string[i] === '%' && string[i + 1] === '2' && string[i + 2] === '0') {
+        returnString += ' '
+        i += 3;
+      }
+      returnString += string[i]
     }
-  ]
+    return returnString;
+  }
+
+  $("#compose-new-tweet").on("submit", function() {
+    let serializedTweet = ( $( this ).serialize() );
+    event.preventDefault();
+
+    if ((serializedTweet.length - 5) <= 0) {
+      alert("Your tweet is too short.");
+      return;
+    }
+
+    if ((spaceNormalizer(serializedTweet).length - 5)  > 140) {
+      alert(`Your tweet is too long.`)
+      return;
+    }
+
+    $.post("/tweets", serializedTweet, function(data) {
+      alert("Serialized tweet received successfully");
+    })
+  })
+
+
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
   
   
   const createTweetElement = function(tweet) {
@@ -44,7 +77,7 @@ $(document).ready(function(){
       </header>
       <span class="tweet-text">${tweet.content.text}</span>
       <footer>
-        <span class="time-since-tweet">${tweet.created_at}</span>
+        <span class="time-since-tweet">${timeago.format(tweet.created_at)}</span>
         <span class="tweet-interacts">
           <i class="fa-solid fa-flag"></i>
           <i class="fa-solid fa-arrows-rotate"></i>
@@ -63,13 +96,19 @@ $(document).ready(function(){
     for (let post of tweets) {
       // calls createTweetElement for each tweet
       let $newTweet = createTweetElement(post);
-      console.log(`${$newTweet}`);
       $(".tweet-container").append($newTweet);
       // takes return value and appends it to the tweets container
     } 
   
   };  
-  renderTweets(data);
-  
+
+  const loadtweets = function() {
+    $.ajax('/tweets', {method: 'GET'})
+    .then(function (loadedTweets) {
+      renderTweets(loadedTweets);
+    });
+  };
+
+  loadtweets();
 
 })
